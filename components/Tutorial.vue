c702978a69cec01cb2e2a05fdf7f7529c2142d7d
muhammmetikbalbayram <muhammetikbalbayram@gmail.com>
2022-04-12 02:46:17 +0300

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="flex justify-between items-center">
+    <div class="text-7xl">Tutorial</div>
+    <div class="text-7xl">Go to İzmir</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
